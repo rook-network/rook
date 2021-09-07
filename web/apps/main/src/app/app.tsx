83d894c6131ab1ae1cd9da6650b1a84c28ba5efe
Callum Waters <cmwaters19@gmail.com>
2021-09-07 07:28:30 +0200

    initialize web framework

---
+++ web/apps/main/src/app/app.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Message } from '@web/api-interfaces';
+
+export const App = () => {
+  const [m, setMessage] = useState<Message>({ message: '' });
+
+  useEffect(() => {
+    fetch('/api')
+      .then((r) => r.json())
+      .then(setMessage);
+  }, []);
+
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome to main!</h1>
+        <img
+          width="450"
+          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+          alt="Nx - Smart, Extensible Build Framework"
+        />
+      </div>
+      <div>{m.message}</div>
+    </>
+  );
+};
+
+export default App;
