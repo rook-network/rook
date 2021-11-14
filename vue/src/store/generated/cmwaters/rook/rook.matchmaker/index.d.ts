39d5e265f2c32f8c047173097ab72f7ea1eff206
Callum Waters <cmwaters19@gmail.com>
2021-11-14 18:36:01 +0100

    remove vue

--- vue/src/store/generated/cmwaters/rook/rook.matchmaker/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Room } from "./module/types/rook/matchmaker/matchmaker";
-import { Rooms } from "./module/types/rook/matchmaker/matchmaker";
-import { Mode } from "./module/types/rook/matchmaker/matchmaker";
-import { Params } from "./module/types/rook/matchmaker/matchmaker";
-export { Room, Rooms, Mode, Params };
-declare const _default;
-export default _default;
